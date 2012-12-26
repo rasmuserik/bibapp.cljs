40167d65b51dde2c89b7bfe21e4036792b4d78af
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-12-26 11:09:14 +0100

    depend on crate

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Mobile/html5 public library app."
   :url "http://github.com/rasmuserik/bibapp"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1552"]]
+                 [org.clojure/clojurescript "0.0-1552"]
+                 [crate "0.2.3"]]
   :plugins [[lein-cljsbuild "0.2.10"]]
   :cljsbuild {
     :builds [{
