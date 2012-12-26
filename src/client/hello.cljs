152951fbc8829c18b1324369d2344211bb777a90
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-12-26 13:59:03 +0100

    frontpage-html

--- src/client/hello.cljs
+++ src/client/hello.cljs
@@ -1,6 +1,23 @@
 (ns hello
   (:require [crate.core :as crate]))
 
+(def frontPage [:div 
+                [:div.header [:span.leftIcon] [:input.searchField] [:span.searchIcon]]
+                [:h1.biblogo "Kardemommeby Bibliotek"]
+                [:div
+                 [:span.patronStatus "Afl. 21/1-13, 4 lån, 1 hjemkomne"]
+                 [:span.openingTimes]]
+                [:ul.news
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]]
+                [:ul.calendar
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]
+                 [:li.item [:span.date "3/1-13"] [:span.content "blah blah blah"]]]])
+(.appendChild js/document.body (crate/html frontPage))
+
+'(
 (defn hello 
   "print a greeting"
   [name]
@@ -11,3 +28,4 @@
 (map hello [124 324])
 (hello 'atom)
 (.appendChild js/document.body (crate/html [:span.test {:style "color: red"} "crate"]))
+  )
